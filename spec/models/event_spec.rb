ecefe345e5114d15181af38b38998db8f06dc3d3
Dan Wagner <daniel.wagner@linkshare.com>
2014-06-05 11:08:49 -0500

    work through test-first free n flop

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
-describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'An Event' do
+  it 'is free if the price is $0' do
+    event = Event.new(price: 0)
+    expect(event.free?).to eql(true)
+  end
+  it 'is not free if the price is not $0' do
+    event = Event.new(price: 10.00)
+    expect(event.free?).to eql(false)
+  end
+  it 'is free if the price is blank' do
+    event = Event.new(price: nil)
+    expect(event.free?).to eql(true)
+  end
 end
