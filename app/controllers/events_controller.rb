0096424db43a404405cf26d7ac7dfa7c4c0da6bd
Dan Wagner <daniel.wagner@linkshare.com>
2014-06-25 16:16:52 -0500

    work through day 19 migrations revisited

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -36,7 +36,7 @@ class EventsController < ApplicationController
   private
   def event_params
     event_params = params.require(:event).
-                  permit(:name, :description, :location, :price, :starts_at)
+                  permit(:name, :description, :location, :price, :starts_at, :image_file_name, :capacity)
   end
 
 end
